f3b03f678e7c90f985a40abef3699f6eefe1c82c
12ephods-source <12ephods@gmail.com>
2026-08-12 19:14:49 -0600

    Add Frost CORE future-capability package

---
+++ src/frost_core/__init__.py
@@ -0,0 +1,35 @@
+"""Frost CORE Ω future-capability runtime primitives."""
+
+from .future_capabilities import (
+    AttentionAction,
+    AttentionDecision,
+    CapabilityActivation,
+    CapabilityRegistry,
+    CapabilityStatus,
+    CognitionRoute,
+    CognitionTask,
+    DelegatedCognitionRouter,
+    EvidenceClass,
+    EvidenceItem,
+    IntelligenceAwareness,
+    PredictiveAttentionManager,
+    Signal,
+    default_registry,
+)
+
+__all__ = [
+    "AttentionAction",
+    "AttentionDecision",
+    "CapabilityActivation",
+    "CapabilityRegistry",
+    "CapabilityStatus",
+    "CognitionRoute",
+    "CognitionTask",
+    "DelegatedCognitionRouter",
+    "EvidenceClass",
+    "EvidenceItem",
+    "IntelligenceAwareness",
+    "PredictiveAttentionManager",
+    "Signal",
+    "default_registry",
+]
