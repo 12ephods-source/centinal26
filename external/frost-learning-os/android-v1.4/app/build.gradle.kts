b19776ef436500123f307a46be963fda56668f79
12ephods-source <12ephods@gmail.com>
2026-08-19 17:37:57 -0600

    Bump Android hotfix package to v1.4.1

--- external/frost-learning-os/android-v1.4/app/build.gradle.kts
+++ external/frost-learning-os/android-v1.4/app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.robertfrost.learningos"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4"
+        versionCode = 15
+        versionName = "1.4.1"
     }
 
     buildTypes {
